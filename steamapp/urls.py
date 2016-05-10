d72cf8b0b5a5e3084bfb4b12e3bbee6b7a4fa8a2
csanahuja <csanahuja10@gmail.com>
2016-05-10 13:52:43 +0200

    Modified urls to make (html/json/xml) extension optional

--- steamapp/urls.py
+++ steamapp/urls.py
@@ -27,12 +27,12 @@ urlpatterns = [
         kwargs={'next_page': '/'}),
 
     # List Players: /steamapp/player.json
-    url(r'^players\.(?P<extension>(json|xml|html))$',
+    url(r'^players\.(?P<extension>(json|xml|html))?$',
         PlayerList.as_view(),
         name='player_list'),
 
     # Player details, ex.: /steamapps/players/<steamid>.json
-    url(r'^players/(?P<pk>\d+)\.(?P<extension>(json|xml|html))$',
+    url(r'^players/(?P<pk>\d+)\.(?P<extension>(json|xml|html))?$',
         PlayerDetail.as_view(),
         name='player_detail'),
 
@@ -42,12 +42,12 @@ urlpatterns = [
         name='player_create'),
 
     # List Games: /steamapp/games.json
-    url(r'^games\.(?P<extension>(json|xml|html))$',
+    url(r'^games\.(?P<extension>(json|xml|html))?',
         GameList.as_view(),
         name='game_list'),
 
     # Game details, ex.: /steamapps/players/<appid>.json
-    url(r'^games/(?P<pk>\d+)(\.(?P<extension>(json|xml|html)))?$',
+    url(r'^games/(?P<pk>\d+)\.(?P<extension>(json|xml|html))?$',
         GameDetail.as_view(),
         name='game_detail'),
 
@@ -57,27 +57,27 @@ urlpatterns = [
         name='game_create'),
 
     # Owned games details, ex.:  /steamapps/ownedgames/<id>.json
-    url(r'^ownedgames/(?P<pk>\d+)\.(?P<extension>(json|xml|html))$',
+    url(r'^ownedgames/(?P<pk>\d+)\.(?P<extension>(json|xml|html))?$',
         OwnedGameDetail.as_view(),
         name='ownedgame_detail'),
 
     # Ban details, ex.:  /steamapps/players/<steamid>/baninfo.json
-    url(r'^banstatus/(?P<pk>\d+)\.(?P<extension>(json|xml|html))$',
+    url(r'^banstatus/(?P<pk>\d+)\.(?P<extension>(json|xml|html))?$',
         BanDetail.as_view(),
         name='ban_detail'),
 
     # List Achievements: /steamapp/achievements.json
-    url(r'^achievements\.(?P<extension>(json|xml|html))$',
+    url(r'^achievements\.(?P<extension>(json|xml|html))?',
         AchievementList.as_view(),
         name='achievement_list'),
 
     # Achievements details, ex.: /steamapps/achievements/<id>.json
-    url(r'^achievements/(?P<pk>\d+)\.(?P<extension>(json|xml|html))$',
+    url(r'^achievements/(?P<pk>\d+)\.(?P<extension>(json|xml|html))?$',
         AchievementDetail.as_view(),
         name='achievement_detail'),
 
     # Owned Achievements details, ex.:  /steamapps/ownedachievements/<id>.json
-    url(r'^ownedachievements/(?P<pk>\d+)\.(?P<extension>(json|xml|html))$',
+    url(r'^ownedachievements/(?P<pk>\d+)\.(?P<extension>(json|xml|html))?$',
         OwnedAchievementDetail.as_view(),
         name='ownach_detail'),
 
